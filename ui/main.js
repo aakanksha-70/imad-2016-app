b9dedcce7fc20bc0fe9ae7a24fda0e0b5fae7f7d
aakanksha-70 <aakanksha17319@gmail.com>
2016-10-10 00:47:55 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,3 +1,9 @@
 console.log('Loaded!');
+
 var element = document.getElementById('main-text');
 element.innerHTML='New value';
+
+var img = documnet.getById('madi');
+img.onclick = function () {
+    img.style.marginlefy = '100px';
+};
