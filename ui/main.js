2e1ffa6ed65b1588356253478ecfbb882e73266d
aakanksha-70 <aakanksha17319@gmail.com>
2016-10-10 16:51:08 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,11 +1,11 @@
-console.log('Loaded!');
+/*console.log('Loaded!');
 
 var element = document.getElementById('main-text');
 element.innerHTML='New value';
 /*function moveLeft (){
     marginRight = marginRight + 1;
     element.style.marginRight = marginRight + 'px';
-}*/
+}
 element.onclick = function () {
     var inter = setInterval(moveRight, 50);
 };
@@ -18,4 +18,21 @@ function moveRight (){
 }
 img.onclick = function () {
     var interval = setInterval(moveRight, 50);
+};*/
+//counter code
+
+
+var button = document.getElementById('counter');
+var counter = 0;
+
+button.onclick = function () {
+  
+  //Make a request to the counter endpoint
+  
+  // Capture the response and store it in a variable
+  
+  // Render the variable in the correct span
+  counter= counter + 1;
+  var span = document.getElementById('count');
+  span.innerHTML = counter.toString();
 };
